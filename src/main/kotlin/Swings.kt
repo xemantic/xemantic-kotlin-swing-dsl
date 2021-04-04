965eedca84f7e8e5c9a962f87435aa6ccdfb1b5e
Kazik Pogoda <morisil@xemantic.com>
2021-04-04 11:53:04 +0200

    textArea support, text change events generalized from JTextField to JTextComponent

--- src/main/kotlin/Swings.kt
+++ src/main/kotlin/Swings.kt
@@ -12,6 +12,7 @@ import java.util.concurrent.TimeUnit
 import javax.swing.*
 import javax.swing.event.DocumentEvent
 import javax.swing.event.DocumentListener
+import javax.swing.text.JTextComponent
 
 private val factory = DefaultJComponentFactory()
 
@@ -43,6 +44,11 @@ fun label(label: String = "", build: (JLabel.() -> Unit)? = null) : JLabel =
 
 fun textField(columns: Int, build: (JTextField.() -> Unit)? = null) : JTextField =
     factory.textField(columns, build)
+
+fun textArea(
+    text: String? = null, rows: Int = 0, columns: Int = 0, build: (JTextArea.() -> Unit)? = null
+): JTextArea =
+    factory.textArea(text, rows, columns, build)
 
 fun <T : JComponent> border(title: String, build: () -> T): T =
     factory.border(title, build)
@@ -86,70 +92,56 @@ interface JComponentFactory {
 
   fun textField(columns: Int, build: (JTextField.() -> Unit)? = null): JTextField
 
+  fun textArea(
+      text: String? = null, rows: Int = 0, columns: Int = 0, build: (JTextArea.() -> Unit)? = null
+  ): JTextArea
+
   fun radioButton(label: String, build: (JRadioButton.() -> Unit)? = null): JRadioButton
 
   fun <T : JComponent> border(title: String, build: () -> T): T
 
 }
 
-class DefaultPanelBuilder<L: LayoutManager>(override val panel: JPanel) : PanelBuilder, JComponentFactory {
+class DefaultPanelBuilder<L: LayoutManager>(override val panel: JPanel) :
+    PanelBuilder, JComponentFactory {
 
-  override fun borderPanel(build: BorderPanelBuilder.() -> Unit): JPanel {
-    val component = factory.borderPanel(build)
+  override fun borderPanel(build: BorderPanelBuilder.() -> Unit): JPanel =
+      add(factory.borderPanel(build))
+
+  override fun flowPanel(build: DefaultPanelBuilder<FlowLayout>.() -> Unit): JPanel =
+      add(factory.flowPanel(build))
+
+  override fun verticalPanel(build: DefaultPanelBuilder<BoxLayout>.() -> Unit): JPanel =
+      add(factory.verticalPanel(build))
+
+  override fun grid(
+      rows: Int, cols: Int, build: DefaultPanelBuilder<GridLayout>.() -> Unit
+  ): JPanel =
+      add(factory.grid(rows, cols, build))
+
+  override fun button(label: String, build: (JButton.() -> Unit)?): JButton =
+      add(factory.button(label, build))
+
+  override fun label(label: String, build: (JLabel.() -> Unit)?): JLabel =
+      add(factory.label(label, build))
+
+  override fun textField(columns: Int, build: (JTextField.() -> Unit)?): JTextField =
+      add(factory.textField(columns, build))
+
+  override fun textArea(
+      text: String?, rows: Int, columns: Int, build: (JTextArea.() -> Unit)?
+  ): JTextArea =
+      add(factory.textArea(text, rows, columns, build))
+
+  override fun radioButton(label: String, build: (JRadioButton.() -> Unit)?): JRadioButton =
+      add(factory.radioButton(label, build))
+
+  override fun <T : JComponent> border(title: String, build: () -> T): T =
+      add(factory.border(title, build))
+
+  fun <T : Component> add(component: T): T {
     panel.add(component)
     return component
-  }
-
-  override fun flowPanel(build: DefaultPanelBuilder<FlowLayout>.() -> Unit): JPanel {
-    val component = factory.flowPanel(build)
-    panel.add(component)
-    return component
-  }
-
-  override fun verticalPanel(build: DefaultPanelBuilder<BoxLayout>.() -> Unit): JPanel {
-    val component =  factory.verticalPanel(build)
-    panel.add(component)
-    return component
-  }
-
-  override fun grid(rows: Int, cols: Int, build: DefaultPanelBuilder<GridLayout>.() -> Unit): JPanel {
-    val component =  factory.grid(rows, cols, build)
-    panel.add(component)
-    return component
-  }
-
-  override fun button(label: String, build: (JButton.() -> Unit)?): JButton {
-    val component = factory.button(label, build)
-    panel.add(component)
-    return component
-  }
-
-  override fun label(label: String, build: (JLabel.() -> Unit)?): JLabel {
-    val component = factory.label(label, build)
-    panel.add(component)
-    return component
-  }
-
-  override fun textField(columns: Int, build: (JTextField.() -> Unit)?): JTextField {
-    val component = factory.textField(columns, build)
-    panel.add(component)
-    return component
-  }
-
-  override fun radioButton(label: String, build: (JRadioButton.() -> Unit)?): JRadioButton {
-    val component = factory.radioButton(label, build)
-    panel.add(component)
-    return component
-  }
-
-  override fun <T : JComponent> border(title: String, build: () -> T): T {
-    val component = factory.border(title, build)
-    panel.add(component)
-    return component
-  }
-
-  fun add(component: Component) {
-    panel.add(component)
   }
 
   @Suppress("UNCHECKED_CAST")
@@ -179,39 +171,39 @@ class DefaultJComponentFactory : JComponentFactory {
     return panel
   }
 
-  override fun grid(rows: Int, cols: Int, build: DefaultPanelBuilder<GridLayout>.() -> Unit): JPanel {
+  override fun grid(
+      rows: Int, cols: Int, build: DefaultPanelBuilder<GridLayout>.() -> Unit
+  ): JPanel {
     val panel = JPanel(GridLayout(rows, cols))
     build(DefaultPanelBuilder(panel))
     return panel
   }
 
-  override fun button(label: String, build: (JButton.() -> Unit)?) : JButton {
-    val component = JButton(label)
-    if (build != null) build(component)
-    return component
-  }
+  override fun button(label: String, build: (JButton.() -> Unit)?) : JButton =
+      make(JButton(label), build)
 
-  override fun label(label: String, build: (JLabel.() -> Unit)?) : JLabel {
-    val component = JLabel(label)
-    if (build != null) build(component)
-    return component
-  }
+  override fun label(label: String, build: (JLabel.() -> Unit)?) : JLabel =
+      make(JLabel(label), build)
 
-  override fun textField(columns: Int, build: (JTextField.() -> Unit)?) : JTextField {
-    val component = JTextField(columns)
-    if (build != null) build(component)
-    return component
-  }
+  override fun textField(columns: Int, build: (JTextField.() -> Unit)?) : JTextField =
+      make(JTextField(columns), build)
 
-  override fun radioButton(label: String, build: (JRadioButton.() -> Unit)?): JRadioButton {
-    val component = JRadioButton(label)
-    if (build != null) build(component)
-    return component
-  }
+  override fun textArea(
+      text: String?, rows: Int, columns: Int, build: (JTextArea.() -> Unit)?
+  ): JTextArea =
+      make(JTextArea(text, rows, columns), build)
+
+  override fun radioButton(label: String, build: (JRadioButton.() -> Unit)?): JRadioButton =
+      make(JRadioButton(label), build)
 
   override fun <T : JComponent> border(title: String, build: () -> T): T {
     val component = build()
     component.border = BorderFactory.createTitledBorder(title)
+    return component
+  }
+
+  private fun <T : JComponent> make(component: T, build: ((T) -> Unit)?): T {
+    if (build != null) build(component)
     return component
   }
 
@@ -271,7 +263,7 @@ val JRadioButton.actionEvents: Observable<ActionEvent>
     emitter.setCancellable { removeActionListener(listener) }
   }
 
-val JTextField.documentChanges: Observable<DocumentEvent>
+val JTextComponent.documentChanges: Observable<DocumentEvent>
   get() = Observable.create { emitter ->
     val listener = object : DocumentListener {
       override fun insertUpdate(e: DocumentEvent) { fireChange(e) }
@@ -283,7 +275,7 @@ val JTextField.documentChanges: Observable<DocumentEvent>
     emitter.setCancellable { document.removeDocumentListener(listener) }
   }
 
-val JTextField.textChanges: Observable<String>
+val JTextComponent.textChanges: Observable<String>
   get() = documentChanges.map { text }
 
 
