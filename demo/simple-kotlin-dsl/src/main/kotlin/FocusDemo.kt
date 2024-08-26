4e2683b9f075c6d14b6c4c5ebfcdbe7cb3f457d8
Kazik Pogoda <morisil@xemantic.com>
2024-08-26 13:19:26 +0200

    additional mouse events and focus events provided as Flows (#6)
    * additional mouse events and focus events provided as Flows
    * build-branch github action

---
+++ demo/simple-kotlin-dsl/src/main/kotlin/FocusDemo.kt
@@ -0,0 +1,52 @@
+/*
+ * This file is part of xemantic-kotlin-swing-dsl - Kotlin goodies for Java Swing.
+ *
+ * Copyright (C) 2024  Kazimierz Pogoda
+ *
+ * xemantic-kotlin-swing-dsl is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * xemantic-kotlin-swing-dsl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with xemantic-kotlin-swing-dsl. If not,
+ * see <https://www.gnu.org/licenses/>.
+ */
+
+package com.xemantic.kotlin.swing.demo
+
+import com.xemantic.kotlin.swing.*
+import java.awt.Dimension
+
+fun main() = MainWindow("Component Focus") {
+  val helpBox = Label {
+    preferredSize = Dimension(200, 0)
+  }
+  BorderPanel {
+    center { helpBox }
+    west {
+      BoxPanel(BoxLayoutAxis.Y) {
+        +Button("action") {
+          focusGains.listen {
+            helpBox.text = "Action help"
+          }
+        }
+        +CheckBox("choice") {
+          focusGains.listen {
+            helpBox.text = "Choice help"
+          }
+        }
+        +Label("label") {
+          focusGains.listen {
+            helpBox.text = "Label help"
+          }
+        }
+      }
+    }
+  }
+}
